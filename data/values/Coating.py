fe63a0bdf2a539dd027b849a6768c30d449d6c0a
Flavia Macovei <diama.m@macovei.eu>
2025-02-02 12:29:53 +0100

    fix typo

--- data/values/Coating.py
+++ data/values/Coating.py
@@ -5,5 +5,5 @@ class Coating():
     def __init__(self, thicknesses: torch.Tensor):
         self.thicknesses = thicknesses
 
-    def get_thickness(self):
+    def get_thicknesses(self):
         return self.thicknesses
