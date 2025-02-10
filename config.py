824e9474188f2c23921a017648d9254cf0b65a45
Flavia Macovei <diama.m@macovei.eu>
2025-02-10 22:04:34 +0100

    add tolerance to config

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import torch
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 num_stacks = 1
 materials_file = "Mirror Design with AI/Simple Designs/Filters/Design narrow Bandpass - optimized.dsg"
+tolerance = 1.0e-3
