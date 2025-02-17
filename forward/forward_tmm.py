1e28ede94e2267e756a16ebc94be6ca3b02db085
Flavia Macovei <diama.m@macovei.eu>
2025-02-17 16:08:29 +0100

    switch to tmm_clean

--- forward/forward_tmm.py
+++ forward/forward_tmm.py
@@ -1,46 +1,19 @@
-import numpy as np
-import torch
-from tmm_fast import coh_tmm as tmm
-import os
-import time
 import sys
 sys.path.append(sys.path[0] + '/..')
 from data.values.ReflectivePropsValue import ReflectivePropsValue
 from data.values.Coating import Coating
-from mirror_transformer.direct_opt import SM, Material, Stack
-from config import device, num_stacks, materials_file
+from config import device, wavelengths, theta
+from tmm_clean.tmm_core import compute_multilayer_optics
 
-def make_stack():
-    sm = SM()
-    materials_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "mirror_transformer")
-    with open(materials_dir + "/" + materials_file, "r", encoding = "ISO-8859-1") as f:
-        for line in f:
-            sm.run(line)
-    materials = [Material(m) for m in sm.materials]
-    st = Stack(sm.front, materials)
-    return st
-
-def create_args(start_wl: int, end_wl: int, steps: int):
-    # prep
-    st = make_stack()
-
-    # make arguments
-    wavelenghts = np.linspace(start_wl, end_wl, steps) * (10 ** (-9))
-    M = torch.tensor(st.make_M(wavelenghts, num_stacks), dtype = torch.float32).to(device)
-    theta = torch.tensor(np.linspace(0, 0, 1) * (np.pi / 180), dtype = torch.float32).to(device)
-    wavelengths = torch.tensor(wavelenghts, dtype=torch.float32).to(device) * 1000000
-
-    return M, theta, wavelengths
-
-def coating_to_reflective_props(coating: Coating, start_wl: int, end_wl: int, steps: int):
+def coating_to_reflective_props(coating: Coating):
     thicknesses = coating.get_thicknesses()
+    refractive_indices = coating.get_refractive_indices()
 
-    M, theta, wavelengths = create_args(start_wl, end_wl, steps)
-    reflective_props_tensor = tmm('p', M, (thicknesses), theta, wavelengths, device=device)['R']
+    reflective_props_tensor = compute_multilayer_optics('s', refractive_indices, thicknesses, theta, wavelengths, device = device)['R']
 
-    steps = reflective_props_tensor.shape[2]
+    steps = reflective_props_tensor.shape[1]
     reflective_props_tensor = reflective_props_tensor.reshape(steps)
 
-    result = ReflectivePropsValue(start_wl, end_wl, reflective_props_tensor)
+    result = ReflectivePropsValue(reflective_props_tensor)
 
     return result
--- ui/UserInput.py
+++ ui/UserInput.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from utils.ObservableList import ObservableList
 import torch
 from config import device, tolerance
@@ -5,20 +7,18 @@ import sys
 sys.path.append(sys.path[0] + '/..')
 from data.values.Region import Region
 from data.values.ReflectivePropsPattern import ReflectivePropsPattern
+from config import wavelengths
 
 class UserInput():
     def __init__(self):
-        self.start_wl = None
-        self.end_wl = None
-        self.steps = None
-        self.step_length = None
         self.regions = ObservableList()
         self.regions.append_action = self.append_action
 
     def append_action(self, region: Region):
         assert region, "Region must not be None."
-        assert self.start_wl <= region.get_start_wl() and self.end_wl >= region.get_end_wl(), "Region must be within specified frame."
-        assert (region.get_start_wl() - self.start_wl) % self.step_length == 0 and (region.get_end_wl() - self.end_wl) % self.step_length == 0, "Region must be multiple of step length."
+        assert self.start_wl <= region.get_start_wl() and self.end_wl >= region.get_end_wl(), "Region must be within frame."
+        assert np.searchsorted(wavelengths, region.get_start_wl()) == region.get_start_wl() and np.searchsorted(wavelengths, region.get_end_wl()) == region.get_end_wl(), "Region must be interval of frame."
+        # assert (region.get_start_wl() - self.start_wl) % self.step_length == 0 and (region.get_end_wl() - self.end_wl) % self.step_length == 0, "Region must be multiple of step length."
         for existing_region in self.regions:
             if not (
                     region.get_start_wl() >= existing_region.get_end_wl() or region.get_end_wl() <= existing_region.get_start_wl()):
@@ -40,21 +40,6 @@ class UserInput():
             print("Invalid input. Please enter a float.")
             return self.read_float_from_input(prompt)
 
-    def read_frame_conditions(self):
-        print("Please specify the start wavelength, end wavelength, and number of steps.")
-        start_wl = self.read_int_from_input("Start wavelength: ")
-        end_wl = self.read_int_from_input("End wavelength: ")
-        if start_wl > end_wl:
-            print("Start wavelength must be less than end wavelength.\nPlease try again.\n")
-            return self.read_frame_conditions()
-        self.start_wl = start_wl
-        self.end_wl = end_wl
-        steps = self.read_int_from_input("Number of steps: ")
-        if steps <= 0:
-            print("Number of steps must be greater than 0.\nPlease try again.\n")
-            return self.read_frame_conditions()
-        self.steps = steps
-        self.step_length = (self.end_wl - self.start_wl) / self.steps
 
     def read_regions(self):
         print("Please specify regions by start wavelength, end wavelength, and target reflectivity.")
@@ -80,18 +65,17 @@ class UserInput():
 
 
     def run(self):
-        self.read_frame_conditions()
         self.read_regions()
 
     def to_reflective_props_pattern(self):
-        lower_bound = torch.zeros(self.steps, device = device)
-        upper_bound = torch.ones(self.steps, device = device)
+        lower_bound = torch.zeros(wavelengths.size, device = device)
+        upper_bound = torch.ones(wavelengths.size, device = device)
 
         for region in self.regions:
-            lower_bound[region.get_start_wl() - self.start_wl:region.get_end_wl() - self.start_wl] = region.get_value()
-            upper_bound[region.get_start_wl() - self.start_wl:region.get_end_wl() - self.start_wl] = region.get_value()
+            lower_bound[region.get_start_wl() - wavelengths[0]:region.get_end_wl() - wavelengths[0]] = region.get_value()
+            upper_bound[region.get_start_wl() - wavelengths[0]:region.get_end_wl() - wavelengths[0]] = region.get_value()
 
         lower_bound = torch.clamp(lower_bound - tolerance / 2, 0, 1)
         upper_bound = torch.clamp(upper_bound + tolerance / 2, 0, 1)
 
-        return ReflectivePropsPattern(self.start_wl, self.end_wl, lower_bound, upper_bound)
+        return ReflectivePropsPattern(lower_bound, upper_bound)
