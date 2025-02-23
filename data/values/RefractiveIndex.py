893270b48128686b63fd27b60a149f0df5df6a29
Flavia <diama.m@macovei.eu>
2025-02-24 00:45:32 +0100

    add tensor-scale rounding functions to Rounder

--- data/values/RefractiveIndex.py
+++ data/values/RefractiveIndex.py
@@ -1,3 +1,9 @@
+import torch
+import sys
+sys.path.append(sys.path[0] + '/..')
+from config import device
+
+
 class RefractiveIndex(float):
     VALUE_SPACE = [0.12, 0.306, 1.38, 1.45, 1.65, 1.8, 2.0, 2.15, 2.25]
     def __init__(self, value):
@@ -12,9 +18,22 @@ class RefractiveIndex(float):
         return RefractiveIndex(min(RefractiveIndex.VALUE_SPACE, key=lambda y: y if y >= x else float('inf')))
 
     @staticmethod
+    def ceil_tensor(x: torch.Tensor):
+        return torch.tensor([RefractiveIndex.ceil(val) for val in x.flatten()], device = device).view(x.shape)
+
+    @staticmethod
     def floor(x: float):
         return RefractiveIndex(max(RefractiveIndex.VALUE_SPACE, key=lambda y: y if y <= x else float('-inf')))
 
     @staticmethod
+    def floor_tensor(x: torch.Tensor):
+        return torch.tensor([RefractiveIndex.floor(val) for val in x.flatten()], device = device).view(x.shape)
+
+
+    @staticmethod
     def round(x: float):
         return RefractiveIndex(min(RefractiveIndex.VALUE_SPACE, key=lambda y: abs(y - x)))
+
+    @staticmethod
+    def round_tensor(x: torch.Tensor):
+        return torch.tensor([RefractiveIndex.round(val) for val in x.flatten()], device = device).view(x.shape)
--- prediction/discretisation/Rounder.py
+++ prediction/discretisation/Rounder.py
@@ -6,6 +6,9 @@ from prediction.discretisation.BaseDiscretiser import BaseDiscretiser
 from data.values.ReflectivePropsPattern import ReflectivePropsPattern
 from data.values.Coating import Coating
 from data.values.RefractiveIndex import RefractiveIndex
+from config import device
+from ui.visualise import visualise
+from forward.forward_tmm import coating_to_reflective_props
 
 class Rounder(BaseDiscretiser):
     def __init__(self, relaxed_solver: BaseRelaxedSolver):
@@ -13,10 +16,11 @@ class Rounder(BaseDiscretiser):
 
     def solve_discretised(self, target: ReflectivePropsPattern):
         relaxed_prediction = self.relaxed_solver.solve_relaxed(target)
+        relaxed_prediction_value = coating_to_reflective_props(relaxed_prediction)
+        visualise(preds = relaxed_prediction_value, filename = "before_rounding")
         return self.round(relaxed_prediction)
 
     def round(self, coating: Coating):
         thicknesses = coating.get_thicknesses()
         refractive_indices = coating.get_refractive_indices()
-        refractive_indices_rounded = torch.tensor([RefractiveIndex.round(x) for x in refractive_indices])
-        return Coating(thicknesses, refractive_indices_rounded)
+        return Coating(thicknesses, RefractiveIndex.round_tensor(refractive_indices))
