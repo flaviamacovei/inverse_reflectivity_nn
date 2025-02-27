fix scaling in generator

data/dataset-generation/BaseGenerator.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(sys.path[0] + '/../..')
 from data.values.Coating import Coating
 from data.values.RefractiveIndex import RefractiveIndex
-from config import tolerance, device
+from config import tolerance, device, min_num_layers, max_num_layers
 
 class BaseGenerator(ABC):
     def __init__(self, num_points: int):
@@ -13,11 +13,11 @@ class BaseGenerator(ABC):
         self.TOLERANCE = tolerance
 
     def make_random_coating(self):
-        num_layers = random.randint(3, 13)
-        thicknesses = torch.rand((num_layers), device = device)
-        thicknesses[0] = float("Inf")
-        thicknesses[-1] = float("Inf")
-        refractive_indices = torch.rand((num_layers))
+        num_layers = random.randint(min_num_layers, max_num_layers)
+        thicknesses = torch.rand((num_layers), device = device) / 1.0e6
+        # thicknesses[0] = 10_000#float("Inf")
+        # thicknesses[-1] = 10_000#float("Inf")
+        refractive_indices = (2.25 - 0.12) * torch.rand((num_layers)) + 0.12
         refractive_indices_rounded = torch.tensor([RefractiveIndex.round(x) for x in refractive_indices], device = device)
         return Coating(thicknesses, refractive_indices_rounded)
 