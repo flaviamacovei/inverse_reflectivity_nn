928b4437653fadb15d79ddc5fed36e97bb7b982b
Flavia <diama.m@macovei.eu>
2025-03-09 16:01:12 +0100

    implement Material class with B and C coefficients

---
+++ data/values/Material.py
@@ -0,0 +1,17 @@
+import torch
+import sys
+sys.path.append(sys.path[0] + '/..')
+from utils.ConfigManager import ConfigManager as CM
+
+class Material:
+    def __init__(self, title: str, B: list[float], C: list[float]):
+        assert len(B) > 0
+        assert len(B) == len(C)
+        self.title = title
+        self.B = B
+        self.C = C
+
+    def refractive_index(self):
+        wl2 = CM().get('wavelengths') ** 2
+        n2 = 1 + sum(b * wl2 / (wl2 - c) for b, c in zip(self.B, self.C))
+        return n2 ** 0.5
