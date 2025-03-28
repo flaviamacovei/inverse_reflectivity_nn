3f44023ea04ded01e2d7981f24fcea1feb9b28d5
Flavia <diama.m@macovei.eu>
2025-03-28 08:45:34 +0100

    implement model testing function

--- evaluation/model_eval.py
+++ evaluation/model_eval.py
@@ -8,14 +8,16 @@ from data.values.ReflectivePropsPattern import ReflectivePropsPattern
 from forward.forward_tmm import coating_to_reflective_props
 from evaluation.loss import match
 from utils.ConfigManager import ConfigManager as CM
+from ui.visualise import visualise
 
-def evaluate(model: BaseModel, batch_size: int):
+def evaluate_model(model: BaseModel):
     print("Evaluating model...")
+    batch_size = 10
     densities = ["complete", "masked", "explicit"]
     total_error = 0
     for density in densities:
         dataloader = init_dataloader(density, batch_size)
-        error = evaluate_per_density(model, dataloader)
+        error = evaluate_per_density(model, dataloader, save_visualisation = False)
         total_error += error
         if CM().get('wandb.log'):
             wandb.log({f"{density}_error": error})
@@ -29,13 +31,23 @@ def init_dataloader(density: str, batch_size: int):
     dataset = torch.load(f"data/datasets/validation/free_{density}_100.pt")
     return DataLoader(dataset, batch_size = batch_size, shuffle = False)
 
-def evaluate_per_density(model: BaseModel, dataloader: DataLoader):
+def evaluate_per_density(model: BaseModel, dataloader: DataLoader, save_visualisation = False):
     error = 0
-    for batch in dataloader:
+    for i, batch in enumerate(dataloader):
         features = batch[0].float().to(CM().get('device'))
         lower_bound, upper_bound = features.chunk(2, dim=1)
         pattern = ReflectivePropsPattern(lower_bound, upper_bound)
         coating = model.predict(pattern)
         preds = coating_to_reflective_props(coating)
+        if save_visualisation:
+            visualise(refs = pattern, preds = preds, filename = "evaluation_{i}")
         error += match(preds, pattern).item()
     return error
+
+def test_model(model: BaseModel):
+    print("Testing model...")
+    batch_size = 10
+    dataset = torch.load(f"data/datasets/test_data/test_data.pt")
+    dataloader = DataLoader(dataset, batch_size = batch_size, shuffle = False)
+    test_error = evaluate_per_density(model, dataloader, save_visualisation = True)
+    print(f"test error: {test_error}")
--- main.py
+++ main.py
@@ -2,75 +2,58 @@ import os
 import time
 import torch
 import wandb
+from data.values.Material import Material
 from forward.forward_tmm import coating_to_reflective_props
-from prediction.relaxation.BaseTrainableRelaxedSolver import BaseTrainableRelaxedSolver
-from prediction.relaxation.GradientSolver import GradientSolver
-from prediction.relaxation.RelaxedMLP import RelaxedMLP
-from prediction.relaxation.RelaxedCNN import RelaxedCNN
-from prediction.discretisation.Rounder import Rounder
-from prediction.discretisation.BranchAndBound import BranchAndBound
+from prediction.BaseTrainableModel import BaseTrainableModel
+from prediction.GradientModel import GradientModel
+from prediction.CNN import CNN
+from prediction.MLP import MLP
 from data.dataloaders.DynamicDataloader import DynamicDataloader
 from data.values.Coating import Coating
 from ui.visualise import visualise
 from ui.FileInput import FileInput
-from data.values.RefractiveIndex import RefractiveIndex
 from data.values.ReflectivePropsPattern import ReflectivePropsPattern
 from utils.ConfigManager import ConfigManager as CM
-from evaluation.model_eval import evaluate
-from data.material_embedding.EmbeddingManager import EmbeddingManager
+from evaluation.model_eval import evaluate_model, test_model
+from data.material_embedding.EmbeddingManager import EmbeddingManager as EM
+from data.dataset_generation.CompletePropsGenerator import CompletePropsGenerator
+from data.dataset_generation.MaskedPropsGenerator import MaskedPropsGenerator
+from data.dataset_generation.ExplicitPropsGenerator import ExplicitPropsGenerator
+from tmm_clean.tmm_core import compute_multilayer_optics
 
 def notify():
     os.system("echo -ne '\007'")
 
-def make_random_pattern():
-    random_thicknesses = torch.rand((1, CM().get('num_layers') - 2), device=CM().get('device')) * 1E-6
-    random_refractive_indices = (2.25 - 0.12) * torch.rand((1, CM().get('num_layers') - 2),
-                                                           device=CM().get('device')) + 0.12
-    random_refractive_indices_rounded = RefractiveIndex.round_tensor(random_refractive_indices)
-    coating = Coating(random_thicknesses, random_refractive_indices_rounded)
-    value = coating_to_reflective_props(coating)
-    lower_bound = torch.clamp(value.get_value() - CM().get('tolerance') / 2, 0, 1).float()
-    upper_bound = torch.clamp(value.get_value() + CM().get('tolerance') / 2, 0, 1).float()
-    return ReflectivePropsPattern(lower_bound, upper_bound)
-
 if __name__ == "__main__":
-    # relaxed_solvers = {
-    #     "gradient": GradientSolver,
-    #     "mlp": RelaxedMLP,
-    #     "cnn": RelaxedCNN
-    # }
-    # discretisers = {
-    #     "rounder": Rounder,
-    #     "b&b": BranchAndBound
-    # }
-    #
-    # RelaxedSolver = relaxed_solvers[CM().get('architecture.relaxed')]
-    # Discretiser = discretisers[CM().get('architecture.discretiser')]
-    #
-    # dataloader = DynamicDataloader(batch_size=CM().get('training.batch_size'), shuffle=False)
-    # dataloader.load_data(CM().get('dataset_files'))
-    #
-    # relaxed_solver = RelaxedSolver(dataloader)
-    # if isinstance(relaxed_solver,BaseTrainableRelaxedSolver):
-    #     relaxed_solver.train()
-    # prediction_engine = Discretiser(relaxed_solver)
-    #
-    # if CM().get('training.evaluate'):
-    #     for batch_size in [1, 10, 20, 50, 100]:
-    #         start_time = time.time()
-    #         evaluate(prediction_engine, batch_size)
-    #         end_time = time.time()
-    #         print(f"Batch size: {batch_size}, time: {end_time - start_time}")
 
-    embedding_manager = EmbeddingManager()
-    # embedding_manager.train()
-    # embedding_manager.save_embeddings()
-    embedding_manager.load_embeddings()
-    # print(embedding_manager)
+    models = {
+        "gradient": GradientModel,
+        "mlp": MLP,
+        "cnn": CNN
+    }
 
-    embedding = torch.tensor([31.49592, 30.524298])
-    print(embedding_manager.map_to_material(embedding))
-    # print("finished")
+    if CM().get('wandb.log'):
+        print("initialising")
+        wandb.init(
+            project=CM().get('wandb.project'),
+            config=CM().get('wandb.config')
+        )
+
+    Model = models[CM().get('architecture')]
+
+    dataloader = DynamicDataloader(batch_size = CM().get('training.batch_size'), shuffle = False)
+    dataloader.load_data(CM().get('dataset_files'))
+
+    model = Model(dataloader)
+    model.load("out/models/model_guided_switch_1000_3_guided_mlp_single.pt")
+
+    if isinstance(model, BaseTrainableModel):
+        model.train()
+
+    if CM().get('training.evaluate'):
+        evaluate_model(model)
+        test_model(model)
+
 
     notify()
 
