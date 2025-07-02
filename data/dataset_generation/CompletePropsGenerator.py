fc1a08122d6b02f7fe4fd9bcdd440971cfb7901e
Flavia <diama.m@macovei.eu>
2025-07-02 09:25:27 +0200

    vectorise CompletePropsGenerator

--- data/dataset_generation/CompletePropsGenerator.py
+++ data/dataset_generation/CompletePropsGenerator.py
@@ -5,6 +5,7 @@ from data.dataset_generation.BaseGenerator import BaseGenerator
 from data.values.Coating import Coating
 from data.values.ReflectivePropsPattern import ReflectivePropsPattern
 from forward.forward_tmm import coating_to_reflective_props
+from data.material_embedding.EmbeddingManager import EmbeddingManager as EM
 
 class CompletePropsGenerator(BaseGenerator):
     """
@@ -17,19 +18,27 @@ class CompletePropsGenerator(BaseGenerator):
     Methods:
         make_point: Generate a 'complete' point.
     """
-    def __init__(self, num_points = 1):
+    def __init__(self, num_points = 1, batch_size: int = 256):
         """Initialise a CompletePropsGenerator instance."""
-        super().__init__(num_points)
+        super().__init__(num_points, batch_size)
 
-    def make_point(self):
+    def make_points(self, num_points: int):
         """
-        Generate a 'complete' point.
+        Generate 'complete' points.
 
         Returns:
             pattern: ReflectivePropsPattern instance where lower bound and upper bound differ by no more than config.tolerance.
             coating: corresponding Coating instance.
         """
-        coating = self.make_random_coating()
+        materials_indices = self.make_materials_choice(num_points)
+        thicknesses = self.make_thicknesses(num_points)
+
+        # make features
+        embedding = self.get_materials_embeddings(materials_indices)
+        coating_encoding = torch.cat([thicknesses[:, :, None], embedding], dim = 2)
+        coating = Coating(coating_encoding)
+
+        # make labels
         reflective_props_tensor = coating_to_reflective_props(coating).get_value()
 
         lower_bound = torch.clamp(reflective_props_tensor - self.TOLERANCE / 2, 0, 1)
