2008fa455975a1783f8c43b1a02a163909ace241
Flavia <diama.m@macovei.eu>
2025-07-04 14:08:41 +0200

    clamp upper and lower errors

--- evaluation/loss.py
+++ evaluation/loss.py
@@ -14,7 +14,9 @@ def match(input: ReflectivePropsValue, target: ReflectivePropsPattern):
         target: reflective properties pattern object.
     """
     upper_error = input.get_value() - target.get_upper_bound()
+    upper_error = upper_error.clamp(min = 0)
     lower_error = target.get_lower_bound() - input.get_value()
+    lower_error = lower_error.clamp(min = 0)
 
     total_error = torch.sum(upper_error ** 2 + lower_error ** 2)
 
