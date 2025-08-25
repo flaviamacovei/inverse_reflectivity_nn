6d7a13ad3ed3e1668a65275df074b23673535f8c
Flavia <diama.m@macovei.eu>
2025-08-25 20:44:57 +0200

    implement MLPGradient
    model extends MLP -> can be trained
    in predict function, prediction is first done on MLP
    then values are used to initialise the GradientModel
    final prediction is prediction of GradientModel

---
+++ prediction/MLPGradient.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import sys
+sys.path.append(sys.path[0] + '/..')
+from prediction.BaseTrainableModel import BaseTrainableModel
+from prediction.MLP import MLP
+from prediction.BaseModel import BaseModel
+from prediction.GradientModel import GradientModel
+from data.dataloaders.BaseDataloader import BaseDataloader
+from utils.ConfigManager import ConfigManager as CM
+from data.values.ReflectivePropsPattern import ReflectivePropsPattern
+
+class MLPGradient(MLP):
+    """
+    Trainable prediction model using an MLP as base.
+
+    Attributes:
+        model: Instance of TrainableMLP.
+    """
+    def __init__(self):
+        """Initialise an MLPGradient instance."""
+        super().__init__()
+        self.gradient = GradientModel()
+
+    def predict(self, target: ReflectivePropsPattern):
+        mlp_prediction = super().predict(target)
+        print(mlp_prediction.get_encoding().shape)
+        self.gradient.initialise(mlp_prediction.get_encoding().detach().flatten())
+        gradient_prediction = self.gradient.predict(target)
+        return gradient_prediction
