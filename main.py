b99aebb8738dc543d1768de225b6d1c3d5dccc7f
Flavia <diama.m@macovei.eu>
2025-08-31 23:23:52 +0200

    implement test-data visualisation in main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import time
 import torch
+from torch.utils.data import DataLoader
 import wandb
 import yaml
 from itertools import product
@@ -10,7 +11,9 @@ from data.values.BaseMaterial import BaseMaterial
 from forward.forward_tmm import coating_to_reflectivity
 from prediction.BaseTrainableModel import BaseTrainableModel
 from prediction.CNN import CNN
+from prediction.GradientModel import GradientModel
 from prediction.MLP import MLP
+from prediction.MLPGradient import MLPGradient
 from prediction.Transformer import Transformer
 from data.dataloaders.DynamicDataloader import DynamicDataloader
 from data.values.Coating import Coating
@@ -26,6 +29,7 @@ from data.dataset_generation.CompletePatternGenerator import CompletePatternGene
 from data.dataset_generation.MaskedPatternGenerator import MaskedPatternGenerator
 from data.dataset_generation.ExplicitPatternGenerator import ExplicitPatternGenerator
 from tmm_clean.tmm_core import compute_multilayer_optics
+from utils.data_utils import get_loaded_model_name
 
 def notify():
     os.system("echo -ne '\007'")
@@ -128,17 +132,57 @@ def overview():
 
     overview.to_csv("out/overview.csv")
 
+def visualise_test_data(architecture: str = None):
+    if architecture is not None:
+        models_types = {
+            'gradient': GradientModel,
+            'mlp': MLP,
+            'cnn': CNN,
+            'transformer': Transformer,
+            'mlp+gradient': MLPGradient,
+        }
+        ModelClass = models_types[architecture]
+        model = ModelClass()
+        if isinstance(model, BaseTrainableModel):
+            model_filename = get_loaded_model_name(architecture)
+            if model_filename is not None:
+                trainable_model = torch.load(model_filename, weights_only = False)
+                trainable_model = trainable_model.to(CM().get('device'))
+                model.model = trainable_model
+            else:
+                raise FileNotFoundError("Provided architecture has no saved model. Run train_model.py first.")
+    dataset = torch.load(f"data/datasets/test_data/test_data.pt", weights_only=False)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    for i, batch in enumerate(dataloader):
+        reflectivity = batch[0]
+        reflectivity = reflectivity.to(CM().get('device'))
+        lower_bound, upper_bound = torch.chunk(reflectivity, 2, -1)
+        target = ReflectivityPattern(lower_bound, upper_bound)
+        if architecture is not None:
+            predicted_coating = model.predict(target)
+            preds = coating_to_reflectivity(predicted_coating)
+        else:
+            preds = None
+        visualise(refs=target, preds=preds, filename=f"test_data_{architecture}_{i}")
 
 if __name__ == "__main__":
     # overview()
+    # dataloader = DynamicDataloader(CM().get('training.dataset_size'), False)
+    # # complete
+    # dataloader.load_leg(0)
+    # reflectivity, coating = dataloader[1]
+    # lower_bound, upper_bound = torch.chunk(reflectivity[None], 2, 1)
+    # pattern = ReflectivityPattern(lower_bound, upper_bound)
+    # label = Coating(coating[None])
+    #
+    # visualise(refs = pattern, filename = "att_test")
+    #
+    # model = Transformer()
+    # loaded_model_path = get_loaded_model_name("transformer")
+    # trainable_model = torch.load(loaded_model_path, weights_only = False)
+    # trainable_model = trainable_model.to(CM().get('device'))
+    # model.model = trainable_model
+    # model.visualise_attention(pattern, label)
 
-    dataloader = DynamicDataloader(CM().get('training.dataset_size'), False)
-    # complete
-    dataloader.load_leg(0)
-    all_complete = dataloader[:][0]
-    # masked
-    dataloader.load_leg(1)
-    all_masked = dataloader[:][0]
-    # explicit
-    dataloader.load_leg(2)
-    all_explicit = dataloader[:][0]
+    visualise_test_data('gradient')
+    print("<3")
