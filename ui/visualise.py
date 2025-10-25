84537ff45856356f1538315e0c0e1b57f1ba1b08
Flavia <diama.m@macovei.eu>
2025-10-25 19:44:46 +0200

    visualise multiple points

--- ui/visualise.py
+++ ui/visualise.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
 from scipy.interpolate import make_interp_spline
 import numpy as np
@@ -9,6 +10,7 @@ from data.values.ReflectivityPattern import ReflectivityPattern
 from data.values.ReflectivityValue import ReflectivityValue
 from utils.ConfigManager import ConfigManager as CM
 from utils.os_utils import get_unique_filename
+from utils.math_utils import largest_prime_factor
 
 
 def visualise_spline(preds: ReflectivityValue = None, refs: ReflectivityPattern = None, filename: str = "visualisation"):
@@ -73,19 +75,36 @@ def visualise(preds: ReflectivityValue = None, refs: ReflectivityPattern = None,
     wavelengths_cpu = CM().get('wavelengths').to("cpu")
     wavelengths_cpu = wavelengths_cpu[min_x:max_x]
 
-    if refs:
-        # plot refs if specified
-        refs = refs.to("cpu")
-        plt.fill_between(wavelengths_cpu, refs.get_lower_bound()[0, min_x:max_x].detach().cpu(), refs.get_upper_bound()[0, min_x:max_x].detach().cpu(), color = '#D4D4D4')
+    if preds is not None:
+        batch_size = preds.get_batch_size()
+    elif refs is not None:
+        batch_size = refs.get_batch_size()
+    else:
+        batch_size = 1
 
-    if preds:
-        # plot preds if specified
-        preds = preds.to("cpu")
-        plt.plot(wavelengths_cpu, preds.get_value()[0, min_x:max_x].detach().cpu(), color='#33638D')
+    num_plots = min(6, batch_size)
+    n_cols = largest_prime_factor(num_plots)
+    n_rows = num_plots // n_cols
 
-    plt.ylim(-0.01, 1.01)
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(n_cols * 3, n_rows * 3))
+    if num_plots == 1:
+        axs = [axs]
+    for i, col in enumerate(axs):
+        for j, ax in enumerate(col):
+            index = i * n_cols + j
+            if refs:
+                # plot refs if specified
+                refs = refs.to("cpu")
+                ax.fill_between(wavelengths_cpu, refs.get_lower_bound()[index, min_x:max_x].detach().cpu(), refs.get_upper_bound()[index, min_x:max_x].detach().cpu(), color = '#D4D4D4')
+            if preds:
+                # plot preds if specified
+                preds = preds.to("cpu")
+                ax.plot(wavelengths_cpu, preds.get_value()[index, min_x:max_x].detach().cpu(), color='#33638D')
+            ax.set_ylim(-0.01, 1.01)
+
+    # plt.ylim(-0.01, 1.01)
     extention = "svg" if svg else "png"
-    plt.savefig(f"out/{filename}.{extention}")
+    fig.savefig(f"out/{filename}.{extention}")
     plt.close()
 
 def visualise_errors(errors: pd.DataFrame, filename: str = "errors_graph", log_scale: bool = False):
@@ -140,4 +159,38 @@ def visualise_matrix(matrix: torch.Tensor, title: str = "visualisation"):
 
     filename = get_unique_filename(f"out/{title}.png")
     plt.savefig(filename)
+    plt.close()
+
+
+def model_snapshot(model: nn.Module, filename: str = "model_snapshot"):
+    num_layers = 0
+    for _, _ in model.named_parameters():
+        num_layers += 1
+    n_cols = largest_prime_factor(num_layers)
+    n_rows = num_layers // n_cols
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(n_cols * 3, n_rows * 3))
+    if num_layers == 1:
+        axs = [axs]
+    for i, col in enumerate(axs):
+        for j, ax in enumerate(col):
+            index = i * n_cols + j
+            name, param = list(model.named_parameters())[index]
+            if len(param.shape) == 1:
+                param = param[None]
+                ax.imshow(param.detach().cpu().numpy(), cmap='viridis', extent=(0, 10, 0, 10), interpolation='none')
+                ax.set_title(name.split('.')[-1])
+                y_shape, x_shape = param.shape
+                x_space = ax.get_xticks()
+                y_space = ax.get_yticks()
+                if len(x_space) > x_shape:
+                    x_space = np.linspace(x_space[0], x_space[-1], x_shape)
+                if len(y_space) > y_shape:
+                    y_space = np.linspace(y_space[0], y_space[-1], y_shape)
+                ax.set_xticks(x_space)
+                ax.set_yticks(y_space)
+                x_ticklabels = np.linspace(0, x_shape, len(x_space))
+                y_ticklabels = np.linspace(0, y_shape, len(y_space))
+                ax.set_xticklabels(x_ticklabels)
+                ax.set_yticklabels(y_ticklabels)
+    fig.savefig(f"out/{filename}.png")
     plt.close()
