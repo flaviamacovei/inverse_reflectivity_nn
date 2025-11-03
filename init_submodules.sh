12b164c5bdbdff1f33e00268a9aade95d5adba7c
Flavia <diama.m@macovei.eu>
2025-11-03 15:49:03 +0100

    add init_submodules script
    initialises submodules for a newly cloned repository

---
+++ init_submodules.sh
@@ -0,0 +1,4 @@
+git rm tmm_clean
+git rm inverse_distance_weighting
+git submodule add https://github.com/ml-uhh/tmm.git tmm_clean/
+git submodule add https://github.com/paulbrodersen/inverse_distance_weighting.git
